632b4a9642edf91e716439d8b42125d6703bedd2
shivangii-sri <shivangisrivastava762@gmail.com>
2024-05-26 18:08:27 +0530

    feat : initial project setup

--- src/main/java/com/springrest/SpringrestApplication.java
+++ src/main/java/com/springrest/SpringrestApplication.java
@@ -1,4 +1,4 @@
-package com.springrest.springrest;
+package com.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
