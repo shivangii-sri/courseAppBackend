67e30d0a5d1e70da14092b70b366f24660766e01
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-05-27 12:40:06 +0530

    feat : Added apis to add, get, update and delete

--- src/main/java/com/springrest/controller/MyController.java
+++ src/main/java/com/springrest/controller/MyController.java
@@ -1,9 +1,52 @@
 package com.springrest.controller;
 
+import com.springrest.entities.Course;
+import com.springrest.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MyController {
 
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping("/courses")
+    public List<Course> getCourses(){
+        return this.courseService.getCourses();
+    }
+
+
+    @GetMapping("/course/{courseId}")
+    public Course getCourse(@PathVariable String courseId){
+        return this.courseService.getCourseById(Long.parseLong(courseId));
+    }
+
+    @PostMapping("/courses")
+    public Course addCourse(@RequestBody Course course){
+        return this.courseService.addCourse(course);
+    }
+
+
+    @PutMapping("/courses")
+    public Course updateCourse(@RequestBody Course course){
+        return this.courseService.updateCourse(course);
+    }
+
+    @DeleteMapping("/courses/{courseId}")
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId){
+
+        try{
+            this.courseService.deleteCourse(Long.parseLong(courseId));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
---
+++ src/main/java/com/springrest/service/CourseService.java
@@ -0,0 +1,42 @@
+package com.springrest.service;
+
+import com.springrest.entities.Course;
+import com.springrest.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+
+    public List<Course> getCourses(){
+        return courseRepository.findAll();
+    }
+
+    public Course getCourseById(Long courseId) {
+        return courseRepository.getReferenceById(courseId);
+    }
+
+    public Course updateCourse(Course course) {
+        courseRepository.save(course);
+        return course;
+    }
+
+    public Course addCourse(Course course) {
+
+        courseRepository.save(course);
+        return course;
+    }
+
+    public void deleteCourse(long courseId) {
+        Course currentCourse=courseRepository.getReferenceById(courseId);
+        courseRepository.delete(currentCourse);
+    }
+}
