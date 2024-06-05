d7d083cfd271fcbd31785a83a2d0234d8581a1eb
shivangii-sri <shivangisrivastava762@gmail.com>
2024-06-06 00:30:28 +0530

    added comments.

--- src/main/java/com/springrest/controller/MyController.java
+++ src/main/java/com/springrest/controller/MyController.java
@@ -1,5 +1,4 @@
 package com.springrest.controller;
-
 import com.springrest.entities.Course;
 import com.springrest.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,31 +16,33 @@ public class MyController {
     @Autowired
     private CourseService courseService;
 
+    //get all courses
     @GetMapping("/courses")
     public List<Course> getCourses(){
         return this.courseService.getCourses();
     }
 
-
+    //get course by id
     @GetMapping("/course/{courseId}")
     public Course getCourseByCourseId(@PathVariable String courseId){
         return this.courseService.getCourseById(Long.parseLong(courseId));
     }
 
+    //add course
     @PostMapping("/courses")
     public Course addCourse(@RequestBody Course course){
         return this.courseService.addCourse(course);
     }
 
-
+    //update course
     @PutMapping("/courses")
     public Course updateCourse(@RequestBody Course course){
         return this.courseService.updateCourse(course);
     }
 
+    //delete course by id
     @DeleteMapping("/courses/{courseId}")
     public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId){
-
         try{
             this.courseService.deleteCourse(Long.parseLong(courseId));
             return new ResponseEntity<>(HttpStatus.OK);
